f3784ad75f2a188bf8e24f4b56130f6c75ae5c7f
Ewen35550 <ewen.herve.35550@gmail.com>
2024-05-02 13:57:27 +0200

    feat(Models) School and Promotion

--- app/Models/School.php
+++ app/Models/School.php
@@ -4,8 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class School extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'address',
+        'numero_declaration',
+    ];
+
+    public function promotions(): HasMany
+    {
+        return $this->hasMany(Promotion::class);
+    }
 }
