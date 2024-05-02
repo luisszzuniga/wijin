20dfb7b108ff29e0734415a0a3d4b7ac0d392eed
Ewen35550 <ewen.herve.35550@gmail.com>
2024-05-02 14:09:43 +0200

    feat(Resources) SchoolResource

---
+++ app/Filament/Resources/SchoolResource.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\SchoolResource\Pages;
+use App\Models\School;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Filament\Forms\Components\TextInput;
+use Filament\Tables\Columns\TextColumn;
+
+class SchoolResource extends Resource
+{
+    protected static ?string $model = School::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required(),
+
+                TextInput::make('address')
+                    ->required(),
+
+                TextInput::make('numero_declaration')
+                    ->integer()
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('address')
+                    ->searchable(),
+                TextColumn::make('numero_declaration')
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListSchools::route('/'),
+            'create' => Pages\CreateSchool::route('/create'),
+            'edit' => Pages\EditSchool::route('/{record}/edit'),
+        ];
+    }
+}
