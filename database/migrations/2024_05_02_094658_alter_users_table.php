ac09dc514c6bd3db7ac4ea96e4b51569cd8671a8
Luis Zuniga <luiszu7779@gmail.com>
2024-05-02 14:52:29 +0200

    est

--- database/migrations/2024_05_02_094658_alter_users_table.php
+++ database/migrations/2024_05_02_094658_alter_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('role_id')->rename('role')->change();
+            $table->string('role_id')->change();
         });
     }
 
