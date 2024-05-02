a60ce0d77e86e1ed3ec748ccd4328ae914ce93f9
Luis Zuniga <luiszu7779@gmail.com>
2024-05-02 14:06:29 +0200

    Promotion

---
+++ app/Filament/Resources/PromotionResource.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PromotionResource\Pages;
+use App\Filament\Resources\PromotionResource\RelationManagers;
+use App\Models\Promotion;
+use Filament\Forms;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class PromotionResource extends Resource
+{
+    protected static ?string $model = Promotion::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make('school_id')
+                    ->required()
+                    ->label('Ecole')
+                    ->options(fn () => \App\Models\School::pluck('name', 'id')),
+
+                TextInput::make('name')
+                    ->required()
+                    ->label('Nom'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable()
+                    ->label('Nom'),
+
+                Tables\Columns\TextColumn::make('school.name')
+                    ->searchable()
+                    ->label('Ecole'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPromotions::route('/'),
+            'create' => Pages\CreatePromotion::route('/create'),
+            'edit' => Pages\EditPromotion::route('/{record}/edit'),
+        ];
+    }
+}
