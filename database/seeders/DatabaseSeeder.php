f88f66de4535f6bc671ed4e68ba46cc5748d7a47
Luis Zuniga <luiszu7779@gmail.com>
2024-05-02 11:56:15 +0200

    Init

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Enums\RoleEnum;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -16,8 +17,11 @@ class DatabaseSeeder extends Seeder
         // User::factory(10)->create();
 
         User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+            'name' => 'Luis Zuniga',
+            'email' => 'luiszu7779@gmail.com',
+            'password' => 'motdepasse',
+            'role' => RoleEnum::Admin->value,
+            'email_verified_at' => now(),
         ]);
     }
 }
