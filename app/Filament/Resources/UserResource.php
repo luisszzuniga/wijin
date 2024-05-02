3e714562d6390edb32e28847db56ccb63244d06e
Luis Zuniga <luiszu7779@gmail.com>
2024-05-02 13:54:31 +0200

    test deploy

--- app/Filament/Resources/UserResource.php
+++ app/Filament/Resources/UserResource.php
@@ -64,7 +64,7 @@ class UserResource extends Resource
             ])
             ->bulkActions([
                 Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\DeleteBulkAction::make(), 
                 ]),
             ]);
     }
