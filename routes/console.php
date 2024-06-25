9276577ca0c1e84478228c3d79e207d40d13e831
Ewen35550 <ewen.herve.35550@gmail.com>
2024-06-25 16:10:07 +0200

    feat: wip ajout de la notification mensuelle de formation

--- routes/console.php
+++ routes/console.php
@@ -3,9 +3,11 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
+use Illuminate\Support\Carbon;
 use \App\Models\Intervention;
 use \App\Models\Formation;
 use \App\Enums\FormationStatusEnum;
+use \App\Models\User;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
@@ -31,7 +33,7 @@ Schedule::call(function () {
                 ->update(['status' => FormationStatusEnum::IN_PROGRESS]);
         }
     }
-})->timezone('Europe/Paris')->dailyAt('16:17');
+})->timezone('Europe/Paris')->dailyAt('7:00');
 
 /**
  * Modifie le statut de la formation en évaluation si c'est la dernière intervention
@@ -54,3 +56,42 @@ Schedule::call(function () {
         }
     }
 })->timezone('Europe/Paris')->dailyAt('18:00');
+
+Schedule::call(function () {
+    $users = User::all();
+    $nextMonth = Carbon::now()->addMonth();
+
+    foreach ($users as $user) {
+        // Récupère les interventions sur le mois prochain
+        $interventions = Intervention::select('interventions.*', 'modules.name', 'schools.name as school_name', 'schools.address')
+            ->join('formations', 'interventions.formation_id', '=', 'formations.id')
+            ->join('modules', 'formations.module_id', '=', 'modules.id')
+            ->join('promotions', 'formations.promotion_id', '=', 'promotions.id')
+            ->join('schools', 'promotions.school_id', '=', 'schools.id')
+            ->where('user_id', $user->id)
+            ->where('date', '>=', $nextMonth->startOfMonth()->format('Y-m-d'))
+            ->where('date', '<=', $nextMonth->endOfMonth()->format('Y-m-d'))
+            ->get();
+
+        if ($interventions->count() === 0) {
+            continue;
+        }
+
+        /**
+         * @example
+         * Le lundi 1er janvier de 8h à 12h chez Nom de l'école au 1 rue de l'école.
+         */
+        $interString = '';
+
+        foreach ($interventions as $inter) {
+            // TODO :
+            //  Gérer les dates en français
+            //  Ajouter les heures
+            $date = date('l t F', strtotime($inter->date));
+
+            $interString .= 'Le ' . $date . ' de ' . ' chez ' . $inter->school_name . ' au ' . $inter->address . '.' . "\n";
+        }
+
+        $this->comment($interString);
+    }
+})->timezone('Europe/Paris')->monthlyOn(1, '7:00');
