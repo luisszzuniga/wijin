a65a9929d6051640ae5b6479a69f9c7546cf3c3e
Luis Zuniga <luiszu7779@gmail.com>
2024-06-26 11:27:31 +0200

    Logo Wijin

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -26,6 +26,7 @@ class AdminPanelProvider extends PanelProvider
     {
         return $panel
             ->default()
+            ->brandLogo('images/logo-wijin.png')
             ->id('admin')
             ->path('admin')
             ->login()
